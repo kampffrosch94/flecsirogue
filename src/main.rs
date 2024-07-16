989b684cf7b140c72913c38130c82539b4a6931c
kampffrosch94 <kampffrosch94@github.com>
2024-07-16 21:55:59 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
-use std::sync::Arc;
+use anyhow::Result;
+use std::{collections::HashMap, ops::Index};
 
 use flecs_ecs::prelude::*;
 use macroquad::prelude::*;
+
+#[derive(Default)]
+pub struct TextureStore {
+    textures: HashMap<String, Texture2D>,
+}
+
+impl TextureStore {
+    pub async fn load_texture(
+        &mut self,
+        path: impl AsRef<str>,
+        name: impl Into<String>,
+    ) -> Result<()> {
+        let texture = load_texture(path.as_ref()).await?;
+        texture.set_filter(FilterMode::Nearest);
+        self.textures.insert(name.into(), texture);
+        Ok(())
+    }
+
+    pub fn get(&self, name: impl AsRef<str>) -> Texture2D {
+        self[name.as_ref()].clone()
+    }
+}
+
+impl Index<&str> for TextureStore {
+    type Output = Texture2D;
+
+    fn index(&self, index: &str) -> &Self::Output {
+        &self.textures[index]
+    }
+}
 
 #[derive(Component, Debug)]
 struct Position {
@@ -10,8 +41,15 @@ struct Position {
 }
 
 #[derive(Component, Debug)]
-struct Player {}
+struct Sprite {
+    texture: Texture2D,
+    x: f32,
+    y: f32,
+    params: DrawTextureParams,
+}
 
+#[derive(Component, Debug)]
+struct Player;
 
 #[macroquad::main("Flecsirogue")]
 async fn main() {
@@ -28,51 +66,67 @@ async fn main() {
 
     set_camera(&camera);
 
-    let rogues_texture = load_texture("assets/32rogues/rogues.png").await.unwrap();
-    rogues_texture.set_filter(FilterMode::Nearest);
+    let mut store = TextureStore::default();
+    store
+        .load_texture("assets/32rogues/rogues.png", "rogues")
+        .await
+        .unwrap();
 
     let w = World::new();
 
-    w.entity_named("Player").set(Position { x: 3, y: 3 });
-    w.system::<(&Position,)>().each(move |(pos,)| {
-	let pos = vec2(32. * pos.x as f32, 32. * pos.y as f32);
-        draw_texture_ex(
-            &rogues_texture,
-            pos.x,
-            pos.y,
-            WHITE,
-            DrawTextureParams {
+    let player = w
+        .entity_named("Player")
+        .set(Position { x: 3, y: 3 })
+        .add::<Player>()
+        .set(Sprite {
+            texture: store.get("rogues"),
+            x: 0.,
+            y: 0.,
+            params: DrawTextureParams {
                 source: Some(Rect::new(0., 0., 32., 32.)),
                 ..Default::default()
             },
+        });
+
+    w.system::<(&Position, &mut Sprite)>()
+        .each(move |(pos, sprite)| {
+            sprite.x = 32. * pos.x as f32;
+            sprite.y = 32. * pos.y as f32;
+        });
+    w.system::<&Sprite>().each(move |sprite| {
+        draw_texture_ex(
+            &sprite.texture,
+            sprite.x,
+            sprite.y,
+            WHITE,
+            sprite.params.clone(),
         );
     });
-
 
     loop {
         clear_background(BLACK);
 
-	/*
-        if (is_key_pressed(KeyCode::W)) {
-            pos.y -= 32.0;
-        }
-        if (is_key_pressed(KeyCode::S)) {
-            pos.y += 32.0;
-        }
-        if (is_key_pressed(KeyCode::A)) {
-            pos.x -= 32.0;
-        }
-        if (is_key_pressed(KeyCode::D)) {
-            pos.x += 32.0;
-        }
-	*/
+        player.get::<&mut Position>(|pos| {
+            if is_key_pressed(KeyCode::W) {
+                pos.y -= 1;
+            }
+            if is_key_pressed(KeyCode::S) {
+                pos.y += 1;
+            }
+            if is_key_pressed(KeyCode::A) {
+                pos.x -= 1;
+            }
+            if is_key_pressed(KeyCode::D) {
+                pos.x += 1;
+            }
+        });
 
         //draw_texture(&tileset, 50., 50., WHITE);
 
         let text = format!("{:?}", camera.screen_to_world(mouse_position().into()));
         draw_text(&text, 20.0, 20.0, 30.0, WHITE);
 
-	w.progress();
+        w.progress();
 
         next_frame().await
     }
