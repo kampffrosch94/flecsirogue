2a743ffddfa28310146444899357b4f19c9178ae
kampffrosch94 <kampffrosch94@github.com>
2024-07-16 20:43:29 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use std::sync::Arc;
+
+use flecs_ecs::prelude::*;
 use macroquad::prelude::*;
+
+#[derive(Component, Debug)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component, Debug)]
+struct Player {}
+
 
 #[macroquad::main("Flecsirogue")]
 async fn main() {
@@ -8,7 +21,7 @@ async fn main() {
         zoom: vec2(scale / screen_width(), scale / screen_height()),
         rotation: 0.,
         offset: vec2(0., 0.0),
-        target: vec2(screen_width()/scale, screen_height()/scale),
+        target: vec2(screen_width() / scale, screen_height() / scale),
         render_target: None,
         viewport: None,
     };
@@ -18,25 +31,11 @@ async fn main() {
     let rogues_texture = load_texture("assets/32rogues/rogues.png").await.unwrap();
     rogues_texture.set_filter(FilterMode::Nearest);
 
-    let mut pos = vec2(32., 32.);
+    let w = World::new();
 
-    loop {
-        clear_background(BLACK);
-
-	if(is_key_pressed(KeyCode::W)) {
-	    pos.y -= 32.0;
-	}
-	if(is_key_pressed(KeyCode::S)) {
-	    pos.y += 32.0;
-	}
-	if(is_key_pressed(KeyCode::A)) {
-	    pos.x -= 32.0;
-	}
-	if(is_key_pressed(KeyCode::D)) {
-	    pos.x += 32.0;
-	}
-
-        //draw_texture(&tileset, 50., 50., WHITE);
+    w.entity_named("Player").set(Position { x: 3, y: 3 });
+    w.system::<(&Position,)>().each(move |(pos,)| {
+	let pos = vec2(32. * pos.x as f32, 32. * pos.y as f32);
         draw_texture_ex(
             &rogues_texture,
             pos.x,
@@ -47,9 +46,33 @@ async fn main() {
                 ..Default::default()
             },
         );
+    });
 
-	let text = format!("{:?}", camera.screen_to_world(mouse_position().into()));
+
+    loop {
+        clear_background(BLACK);
+
+	/*
+        if (is_key_pressed(KeyCode::W)) {
+            pos.y -= 32.0;
+        }
+        if (is_key_pressed(KeyCode::S)) {
+            pos.y += 32.0;
+        }
+        if (is_key_pressed(KeyCode::A)) {
+            pos.x -= 32.0;
+        }
+        if (is_key_pressed(KeyCode::D)) {
+            pos.x += 32.0;
+        }
+	*/
+
+        //draw_texture(&tileset, 50., 50., WHITE);
+
+        let text = format!("{:?}", camera.screen_to_world(mouse_position().into()));
         draw_text(&text, 20.0, 20.0, 30.0, WHITE);
+
+	w.progress();
 
         next_frame().await
     }
